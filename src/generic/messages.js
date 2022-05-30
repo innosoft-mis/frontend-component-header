f4c17e5df154eaeed8ef73117377c84f88a3c68f
titlethanason <titlethanason@gmail.com>
2022-05-30 14:53:09 +0700

    :beetle: (th) remove help in nav

--- src/generic/messages.js
+++ src/generic/messages.js
@@ -3,12 +3,12 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   registerSentenceCase: {
     id: 'general.register.sentenceCase',
-    defaultMessage: 'Register',
+    defaultMessage: 'ลงทะเบียน',
     description: 'Text in a button, prompting the user to register.',
   },
   signInSentenceCase: {
     id: 'general.signIn.sentenceCase',
-    defaultMessage: 'Sign in',
+    defaultMessage: 'เข้าสู่ระบบ',
     description: 'Text in a button, prompting the user to log in.',
   },
 });
--- src/learning-header/messages.js
+++ src/learning-header/messages.js
@@ -3,37 +3,37 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 const messages = defineMessages({
   dashboard: {
     id: 'header.menu.dashboard.label',
-    defaultMessage: 'Dashboard',
+    defaultMessage: 'แดชบอร์ด',
     description: 'The text for the user menu Dashboard navigation link.',
   },
   help: {
     id: 'header.help.label',
-    defaultMessage: 'Help',
+    defaultMessage: 'ช่วยเหลือ',
     description: 'The text for the link to the Help Center',
   },
   profile: {
     id: 'header.menu.profile.label',
-    defaultMessage: 'Profile',
+    defaultMessage: 'โปรไฟล์',
     description: 'The text for the user menu Profile navigation link.',
   },
   account: {
     id: 'header.menu.account.label',
-    defaultMessage: 'Account',
+    defaultMessage: 'บัญชี',
     description: 'The text for the user menu Account navigation link.',
   },
   orderHistory: {
     id: 'header.menu.orderHistory.label',
-    defaultMessage: 'Order History',
+    defaultMessage: 'ประวัติคำสั่งซื้อ',
     description: 'The text for the user menu Order History navigation link.',
   },
   skipNavLink: {
     id: 'header.navigation.skipNavLink',
-    defaultMessage: 'Skip to main content.',
+    defaultMessage: 'ข้ามไปที่เนื้อหาหลัก',
     description: 'A link used by screen readers to allow users to skip to the main content of the page.',
   },
   signOut: {
     id: 'header.menu.signOut.label',
-    defaultMessage: 'Sign Out',
+    defaultMessage: 'ลงชื่อออก',
     description: 'The label for the user menu Sign Out action.',
   },
 });
